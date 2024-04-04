470909764597443bcf54e6a6a54c70c67dc77eec
Vikhyat Garg <149285694+vikhyat9690@users.noreply.github.com>
2024-04-04 20:08:41 +0530

    script.js

--- Day 3 JS/JS-intro/script.js
+++ Day 3 JS/JS-intro/script.js
@@ -1,55 +1,55 @@
-// fetch("https://jsonplaceholder.typicode.com/posts", { method: "GET" }).then((res) => res.json()).then((e) => {
-//     console.log(e);
-// })
+fetch("https://jsonplaceholder.typicode.com/posts", { method: "GET" }).then((res) => res.json()).then((e) => {
+    console.log(e);
+})
 
-// async function fetchData() {
+async function fetchData() {
 
-//     const Response = await fetch("https://jsonplaceholder.typicode.com/posts");
-//     const Data = await Response.json();
-//     console.log(Data);
-// }
+    const Response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const Data = await Response.json();
+    console.log(Data);
+}
 
-// fetchData();
+fetchData();
 
-// const btn = document.getElementById("btn");
-// let mode = 'dark';
-// btn.addEventListener("click", () => {
+const btn = document.getElementById("btn");
+let mode = 'dark';
+btn.addEventListener("click", () => {
 
-//     if(mode === "light") {
-//         document.body.style.backgroundColor = "black";
-//         document.body.style.color = "white";
-//         mode = "dark";
-//     } else {
-//         document.body.style.backgroundColor = "white";
-//         document.body.style.color = "black";
-//         mode = "light";
-//     }
+    if(mode === "light") {
+        document.body.style.backgroundColor = "black";
+        document.body.style.color = "white";
+        mode = "dark";
+    } else {
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
+        mode = "light";
+    }
     
-// })
+})
 
-//function declaration
-// function print() {
-//     console.log('--');
-// }
+function declaration
+function print() {
+    console.log('--');
+}
 
-// //function assignment (named)
-// const a = function print() {
-//     console.log('--');
-// }
+//function assignment (named)
+const a = function print() {
+    console.log('--');
+}
 
-// //function assignment (anonymous)
-// const b = function () {
-//     console.log('--');
-// }
+//function assignment (anonymous)
+const b = function () {
+    console.log('--');
+}
 
-// //Object 
-// const obj = {
-//     1 : 'one',
-//     'name': 'Vikhyat',
-//     12 : 'twelve',
-// };
+//Object 
+const obj = {
+    1 : 'one',
+    'name': 'Vikhyat',
+    12 : 'twelve',
+};
 
-// console.log(obj);
+console.log(obj);
 
 //prompt
 const x = prompt('Please enter a number : ')
